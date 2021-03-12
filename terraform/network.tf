84ae3afad2d5ed3f606001c36d6932c98350ba1a
bruno.brandao <bruno.brandao@n26.com>
2021-03-12 23:11:43 +0100

    Custom subnet

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,5 +1,12 @@
 resource "google_compute_network" "vpc_network" {
   name = "vpc-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "vpc_network_subnetwork" {
+  name          = "vpc-network-subnetwork"
+  ip_cidr_range = "10.128.0.0/9"
+  network       = "${google_compute_network.vpc_network.name}"
 }
 
 resource "google_compute_firewall" "vpc_network_firewall" {
